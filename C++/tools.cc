19231e87fd23a6bcbbbd16d274c0c71b395bf959
alpha1426 <48439056+alpha1426@users.noreply.github.com>
2019-04-12 20:02:12 +0200

    Add files via upload
    tout fonctionne comme avant, j'ai juste ajouter de nouvelle classe, et de nouveaux attributs /héritages/méthodes
    
    nouvelle classe: Couleur avec 3 attributs : (double red, doube green, double blue)
    nouvelle classe : Form avec un attribut couleur
    
    modification classe rectangle: 
    
    ajout 2 nouveaux attributs: double hauteur, double largeur
    hérite de Form
    ajout d'un nouveau constructeur: Rectangle(Point p, double hauteur, double largeur, couleur c)       ps: p = coin supérieur gauche
    
    nouvelle classe : Rond
    
    -hérite de Form
    -possède 2 attributs : Point centre_, double rayon_
    -un constructeur : Rond(Point centre, double rayon, Couleur c)
    
    nouvelle classe: Arc
    -possède 3 attributs : double rayon_ext_, double rayon_int_, double fraction_    ps: fraction = fraction du cercle que fait l'arc
    -hérite de Form
    -possède un constructeur Arc(double r_ext, r_int, double fraction, Couleur c)

--- C++/tools.cc
+++ C++/tools.cc
@@ -3,6 +3,20 @@
 #include <algorithm>
 using namespace std;
 
+Couleur::Couleur (double r, double g, double b)
+		: red_(r), green_(g), blue_(b)
+		{}
+		
+void Couleur::red(double r){red_ = r;}
+double Couleur::red(){return red_;}
+		
+void Couleur::green(double g){green_ = g;}
+double Couleur::green(){return green_;}
+		
+void Couleur::blue(double b){blue_ = b;}
+double Couleur::blue(){return blue_;}
+
+
 Point::Point()
 		: x_(0),y_(0)
 		{}
@@ -18,18 +32,33 @@ void Point::y(double new_y){y_ = new_y;}
 double Point::x(){return x_;}
 double Point::y(){return y_;}		
 
+Form::Form()
+		:couleur_(0,0,0)
+		{}
+Form::Form(Couleur c)
+		:couleur_(c)
+		{}
+		
+		
+void Form::couleur(Couleur c){couleur_ = c;}
+Couleur Form::couleur(){return couleur_;}
+
 
 
 Rectangle::Rectangle ()
-		: p1_(0,0)
+		: Form(), p1_(0,0)
 		{}
 		
 Rectangle::Rectangle (Point a, Point b)
-		: p1_(a), p2_(a.x(),b.y()),p3_(b),p4_(a.y(),b.x())
+		: Form(), p1_(a), p2_(a.x(),b.y()),p3_(b),p4_(a.y(),b.x())
 		{}
 		
 Rectangle::Rectangle (Point a, Point b, Point c, Point d)
-		: p1_(a), p2_(b), p3_(c), p4_(d)
+		: Form(), p1_(a), p2_(b), p3_(c), p4_(d)
+		{}
+		
+Rectangle::Rectangle (Point a, double h, double l, Couleur c)
+		: Form(c), p1_(a), hauteur_(h), largeur_(l)
 		{}
 		
 
@@ -37,6 +66,9 @@ Point Rectangle::c1(){return p1_;}
 Point Rectangle::c2(){return p2_;}
 Point Rectangle::c3(){return p3_;}
 Point Rectangle::c4(){return p4_;}
+double Rectangle::largeur(){return largeur_;}
+double Rectangle::hauteur(){return hauteur_;}
+
 
 bool Rectangle::appartient(Point a)
 {
@@ -50,7 +82,32 @@ bool Rectangle::appartient(Point a)
 	else {return false;}
 }
 		
-	
+Rond::Rond(Point p, double r, Couleur c)
+		: Form(c), centre_(p), rayon_(r)
+		{}
+		
+void Rond::centre(Point p){centre_ = p;}
+Point Rond::centre(){return centre_;}
+
+void Rond::rayon(double r){rayon_ = r;}
+double Rond::rayon(){return rayon_;}
+
+
+Arc::Arc(double r_ext, double r_int, double f, Couleur c)
+		: Form(c), rayon_ext_(r_ext), rayon_int_(r_int), fraction_(f)
+		{}
+		
+void Arc::rayon_ext(double r){rayon_ext_ = r;}
+double Arc::rayon_ext(){return rayon_ext_;}
+
+void Arc::rayon_int(double r){rayon_int_ = r;}
+double Arc::rayon_int(){return rayon_int_;}
+
+void Arc::fraction(double f){fraction_ = f;}
+double Arc::fraction(){return fraction_;}
+
+
+
 		
 double ecart(Point a, Point b){
 	double d(0);
--- C++/tools.h
+++ C++/tools.h
@@ -3,10 +3,32 @@
 
 #include "define.h"
 
-
+class Couleur
+{
+	protected:
+		double red_;
+		double green_;
+		double blue_;
+		
+	public:
+		Couleur (double r, double g, double b);
+		
+		void red(double r);
+		double red();
+		
+		void green(double g);
+		double green();
+		
+		void blue(double b);
+		double blue();
+		
+};
+		
+		
+		
 class Point
 {
-	private:
+	protected:
 		double x_;
 		double y_;
 		
@@ -23,13 +45,32 @@ class Point
 		
 };
 
-class Rectangle
+
+
+class Form
 {
-	private:
+	protected:
+		Couleur couleur_;
+		
+	public:
+		Form();
+		Form(Couleur c);
+		
+		void couleur(Couleur c);
+		Couleur couleur();
+};
+
+		
+
+class Rectangle : public Form
+{
+	protected:
 		Point p1_;   //coordonnées des 4 coins du rectangle
 		Point p2_;
 		Point p3_;
 		Point p4_;
+		double hauteur_;
+		double largeur_;
 		
 	public:
 		Rectangle ();
@@ -38,16 +79,57 @@ class Rectangle
 		
 		Rectangle (Point a, Point b, Point c, Point d);
 		
+		Rectangle (Point a, double h, double l, Couleur c);
+		
 		Point c1();   //donne les coordonnée des 4 coins
 		Point c2();
 		Point c3();
 		Point c4();
+		double largeur();
+		double hauteur();
+		
 		
 		bool appartient(Point a);   //renvoie true si le point est dans 
 };                                  //le rectangle
 		
+class Rond : public Form
+{
+	protected:
+		Point centre_;
+		double rayon_;
 		
-
+		
+	public:
+		Rond(Point p, double r, Couleur c);
+		
+		void centre(Point p);
+		Point centre();
+		
+		void rayon(double r);
+		double rayon();
+};
+		
+class Arc : public Form
+{
+	protected:
+		double rayon_ext_;
+		double rayon_int_;
+		double fraction_;
+		
+	public:
+		Arc(double r_ext, double r_int, double f, Couleur c);
+		
+		void rayon_ext(double r);
+		double rayon_ext();
+		
+		void rayon_int(double r);
+		double rayon_int();
+		
+		void fraction(double f);
+		double fraction();
+		
+};
+		
 
 double ecart(Point a,Point b);    //donne la distance entre 2 point
 
